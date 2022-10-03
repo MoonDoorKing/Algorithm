b93218e0be5f2e345411f0d5cbff74a1d9a1cc13
MoonDoorKing <rizingsniper@kakao.com>
2022-10-03 11:16:43 +0900

    Update 머쓱이보다키큰사람.java

--- src/main/java/프로그래머스/머쓱이보다키큰사람.java
+++ src/main/java/프로그래머스/머쓱이보다키큰사람.java
@@ -1,4 +1,12 @@
 package 프로그래머스;
 
 public class 머쓱이보다키큰사람 {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        for(int i : array) {
+            if(i > height)
+                answer++;
+        }
+        return answer;
+    }
 }
