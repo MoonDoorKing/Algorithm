21248ede0342408a73b6017ea7f6a92a4ff6db2d
MoonDoorKing <rizingsniper@kakao.com>
2023-03-31 21:20:05 +0900

    알고리즘 리마인드

---
+++ src/main/java/백준/백준25628번.java
@@ -0,0 +1,18 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 백준25628번 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.min(a/2,b));
+    }
+}
